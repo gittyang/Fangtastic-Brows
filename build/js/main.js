bccf550e7634f9977a4eed518ae9a578d100daf7
Tou Toua Yang <57574477+gittyang@users.noreply.github.com>
2020-08-02 15:18:45 -0500

    16th commit: FAQ js done

--- build/js/main.js
+++ build/js/main.js
@@ -1,11 +1,31 @@
 
 
 // Parallax Effects
-  const parallax = document.querySelector('.landing-parallax');
-  console.log(parallax);
+const parallax = document.querySelector('.landing-parallax');
+console.log(parallax);
 
-  window.addEventListener('scroll', () => {
-    let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = offset * -.4 + "px";
+window.addEventListener('scroll', () => {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * -.4 + "px";
+});
+
+
+// FAQ
+
+$(document).ready(function() {
+
+  $('.faq-wrapper').on('click', function(event) {
+
+    $(this).parent().next().slideToggle('slow', function(){
+      console.log('toggle worked');
+    });
+
+    $(this).children('.faq-plus').toggleClass('hide');
+    console.log('hide worked');
+
   });
+
+});
+
+
 
