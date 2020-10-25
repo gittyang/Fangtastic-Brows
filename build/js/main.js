c2aeb7acf307e2141542130edf7a9eca3f51a53b
Tou Toua Yang <ttyang251@gmail.com>
2020-10-24 23:13:05 -0500

    25th commit: media queries for faqs page sm devices + js changes to slideToggle (js bug - displays all faqs after resize)

--- build/js/main.js
+++ build/js/main.js
@@ -10,20 +10,81 @@ window.addEventListener('scroll', () => {
 });
 
 
-// FAQ
 
+// FAQ
 $(document).ready(function() {
 
   $('.info-item-heading').on('click', function(event) {
 
-    $(this).next().slideToggle('slow', function(){
-      console.log('toggle worked');
-    });
+    if (window.matchMedia('(min-width: 576px)').matches) {
 
-    $(this).children().children('.faq-plus').toggleClass('hide');
-    console.log('hide worked');
+      // this toggles the slide in slide out animation
+      $(this).next().slideToggle(function(){
+        console.log('toggle worked');
+      });
+  
+      // this toggles the minus and plus sign
+      $(this).children().children('.faq-plus').toggleClass('hide');
+      console.log('hide worked');
+
+    } else {
+      console.log('screen too small');
+    }
 
   });
+
+
+
+  // window.onresize = function(event) {
+    
+  //   if (window.matchMedia('(max-width: 576px)').matches) {
+
+  //     $('.info-item-body').css('display', 'block');
+  //     console.log('shown')
+
+  //   } else {
+
+  //     $('.info-item-body').css('display', 'none');
+  //     console.log('hid')
+
+  //   }
+
+  // }
+
+
+
+  window.onresize = function(event) {
+  
+    if (window.matchMedia('(min-width: 576px)').matches ) {
+
+      console.log('do nothing');
+  
+    } else if (window.matchMedia('(max-width: 576px)').matches) {
+
+      // when window is resized and if screen is below 576px
+
+      // 1. if a faq icon is open (-), then close to (+)
+      if( $('.faq-plus').hasClass('hide') ) {
+
+        $('.faq-plus').removeClass('hide');
+        
+      }
+
+      // 2. display the faq body paragraph
+      $('.info-item-body').css('display', 'block');
+      console.log('shown')
+
+    } else {
+      
+      // currently not working
+      $('.info-item-body').css('display', 'none');
+      console.log('else');
+
+    }
+
+  };
+
+
 
 });
 
