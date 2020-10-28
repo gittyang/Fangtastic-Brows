3b1db980d52f15ced5c5bbde19f3ed27f6b23ae6
Tou Toua Yang <ttyang251@gmail.com>
2020-10-27 23:45:01 -0500

    26th commit: js resize event fixed (js bug - sBodies elements saved even after being closes)

--- build/js/main.js
+++ build/js/main.js
@@ -2,7 +2,7 @@
 
 // Parallax Effects
 const parallax = document.querySelector('.landing-parallax');
-console.log(parallax);
+// console.log(parallax);
 
 window.addEventListener('scroll', () => {
   let offset = window.pageYOffset;
@@ -14,18 +14,23 @@ window.addEventListener('scroll', () => {
 // FAQ
 $(document).ready(function() {
 
+  // Slide Out Animation Toggle
   $('.info-item-heading').on('click', function(event) {
 
     if (window.matchMedia('(min-width: 576px)').matches) {
 
       // this toggles the slide in slide out animation
-      $(this).next().slideToggle(function(){
+      const $body = $(this).next();
+      console.log('hi');
+      $body.slideToggle(function(){
+        $(this).removeClass('hide');
+        $(this).addClass('show');
         console.log('toggle worked');
       });
   
       // this toggles the minus and plus sign
       $(this).children().children('.faq-plus').toggleClass('hide');
-      console.log('hide worked');
+      // console.log('hide worked');
 
     } else {
       console.log('screen too small');
@@ -34,59 +39,147 @@ $(document).ready(function() {
   });
 
 
+  // On page load of sm, display body
+  if ( window.matchMedia('(max-width: 576px)').matches ) {
 
-  // window.onresize = function(event) {
-    
-  //   if (window.matchMedia('(max-width: 576px)').matches) {
+    // on page load, if window screen is less than 576px
 
-  //     $('.info-item-body').css('display', 'block');
-  //     console.log('shown')
+    // display the faq body paragraph
+    $('.info-item-body').css('display', 'block');
+    console.log('shown')
 
-  //   } else {
+  }
 
-  //     $('.info-item-body').css('display', 'none');
-  //     console.log('hid')
+  // Set timeout for resize event
+  var resizeId;
+  $(window).resize(function() {
 
-  //   }
+    clearTimeout(resizeId);
+    resizeId = setTimeout(resize, 10)
 
-  // }
+  });
 
+  // On resize to sm, display body
+  function resize () {
 
-
-  window.onresize = function(event) {
-  
-    if (window.matchMedia('(min-width: 576px)').matches ) {
-
-      console.log('do nothing');
-  
-    } else if (window.matchMedia('(max-width: 576px)').matches) {
+    if ( window.matchMedia('(max-width: 576px)').matches ) {
 
       // when window is resized and if screen is below 576px
 
-      // 1. if a faq icon is open (-), then close to (+)
+      // if a faq icon is open (-), then close to (+)
       if( $('.faq-plus').hasClass('hide') ) {
 
         $('.faq-plus').removeClass('hide');
         
       }
 
-      // 2. display the faq body paragraph
+      // display the faq body paragraph
       $('.info-item-body').css('display', 'block');
-      console.log('shown')
+      console.log('shown after resize')
 
     } else {
-      
-      // currently not working
-      $('.info-item-body').css('display', 'none');
-      console.log('else');
+
+      // otherwise, for m screens or higher, display original state
+
+      const show = document.querySelectorAll('.show');
+      // console.log(show);
+      for (i = 0; i < show.length; i ++) {
+        const sBodies = show[i];
+        console.log(sBodies);
+      }
+      $('.show').css('display', 'block');
+
+      const hide = document.querySelectorAll('.hide');
+      // console.log(hide);
+      for (i = 0; i < hide.length; i ++) {
+        const hBodies = hide[i];
+        console.log(hBodies);
+      }
+      $('.hide').css('display', 'none');
 
     }
 
   };
 
-
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const $body = $('.info-item-body');
+// // console.log($body);
+// // const $displayedBody = $('.info-item-body').addClass('show');
+// // console.log($displayedBody); 
+
+// // const $dBody = $('.show');
+
+// // .filter(function () { 
+// //   return $('.info-item-body').hasClass('show');
+// // });
+
+// // console.log($dBody);
+
+
+// const $head = $('.info-item-heading');
+// console.log($head);
+
+// const $shows = $('.shows');
+// console.log($shows);
+
+// $head.click(function(){
+//   const $dBody = $('.show');
+//   console.log($dBody);
+//   // console.log($body);
+// });
+
 
 
 
