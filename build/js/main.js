86f12f5bc73d692b64dc19c9b0578e16f515a9ad
Tou Toua Yang <ttyang251@gmail.com>
2020-10-30 18:13:45 -0500

    31st commit: jquery added to navbar drop down

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,6 @@
 
 
+
 // Parallax Effects
 const parallax = document.querySelector('.landing-parallax');
 
@@ -7,6 +8,27 @@ window.addEventListener('scroll', () => {
   let offset = window.pageYOffset;
   parallax.style.backgroundPositionY = offset * -.4 + "px";
 });
+
+
+
+// Navbar Drop Down Menu
+if (window.matchMedia('(max-width: 576px)').matches)  {
+  console.log('sm devices')
+
+  const $dropDown = document.querySelector('.nav-drop-down');
+  console.log($dropDown);
+  
+  $('.nav-drop-down').on('click', function(event) {
+  
+    const $navLinks = $('.nav-links');
+    $navLinks.slideToggle(function() {
+      console.log('toggle worked for nav');
+    });
+  
+  });
+   
+}
+
 
 
 
